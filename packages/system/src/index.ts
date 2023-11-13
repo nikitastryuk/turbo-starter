850453c6facaeb2a7536801d580496fe33d2eb8d
Nikita Striuk <nikitastryuk@gmail.com>
2023-11-13 22:24:51 +0200

    auth forms wip

--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -1,1 +1,5 @@
-export { Button } from './components/ui/button';
+export * from './components/ui/button';
+export * from './components/ui/form';
+export * from './components/ui/label';
+export * from './components/ui/input';
+export { cn } from './lib/utils';
